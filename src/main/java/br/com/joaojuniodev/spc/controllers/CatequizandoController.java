2a58bf4cf551c223e85fac987003614c47eb1a33
joaojuniodev <=>
2026-03-13 22:35:36 -0300

    teste 2

--- src/main/java/br/com/joaojuniodev/spc/controllers/CatequizandoController.java
+++ src/main/java/br/com/joaojuniodev/spc/controllers/CatequizandoController.java
@@ -33,7 +33,7 @@ public class CatequizandoController {
     }
 
     @GetMapping(
-        value = "/findByEtapaId/{etapaId}",
+        value = "/findByEtapaId/{etapaIdD}",
         produces = { MediaType.APPLICATION_JSON_VALUE }
     )
     public ResponseEntity<List<CatequizandoResponseDTO>> findByEtapaId(@PathVariable Long etapaId) {
